701f9421b5e3caacda8328e15485d6b5833ec7e4
Serginho <madridserginho@hotmail.com>
2020-11-24 11:32:19 +0100

    add work aliases

---
+++ ohmyzsh-custom/plugins/work/work.plugin.zsh
@@ -0,0 +1,9 @@
+alias cdtw='cd && cd tulotero/tulotero-web'
+alias cdtwl='cd && cd tulotero/tulotero-web-landing'
+alias ycse='yarn cli:serve:es'
+alias ycsm='yarn cli:serve:mx'
+alias ybt='yarn bazel test'
+alias ybr='yarn bazel run'
+alias yl='yarn lint'
+alias yi='yarn install'
+
