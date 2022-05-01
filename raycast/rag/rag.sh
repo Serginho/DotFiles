0432056bf1ec98550be5f48efc3f10f123bdc34f
Serginho <yo@sergiocancelo.es>
2022-05-01 20:10:07 +0200

    add rag to search terms

---
+++ raycast/rag/rag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Search in RAG
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+
+# Optional parameters:
+# @raycast.icon images/rag.jpg
+# @raycast.argument1 { "type": "text", "placeholder": "query", "percentEncoded": true }
+
+open "https://academia.gal/dicionario/-/termo/busca/$1"
