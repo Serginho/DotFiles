84a09ef24d3235014bf2c8b500e0b5d72b91ecd0
Serginho <yo@sergiocancelo.es>
2023-11-08 10:02:37 +0100

    fix: docker aliases

--- ohmyzsh-custom/plugins/cdocker/cdocker.plugin.zsh
+++ ohmyzsh-custom/plugins/cdocker/cdocker.plugin.zsh
@@ -1,7 +1,21 @@
 alias dbl='docker build -t'
-alias dsaf="docker start $(docker ps | awk 'NR==2 {print $1}')"
+
+function __dsaf() {
+  docker start $(docker ps | awk 'NR==2 {print $1}')
+}
+
+function __dsof() {
+  docker stop $(docker ps | awk 'NR==2 {print $1}')
+}
+
+function __drmf() {
+  docker rm $(docker ps -a | awk 'NR==2 {print $1}')
+}
+
+
+alias dsaf=__dsaf
 alias dsoa='docker stop $(docker ps -q)'
-alias dsof="docker stop $(docker ps | awk 'NR==2 {print $1}')"
+alias dsof=__dsof
 alias deit='docker exec -it'
 alias drmi='docker rmi'
-alias drmf="docker rm $(docker ps -a | awk 'NR==2 {print $1}')"
+alias drmf=__drmf
