e50db51d7f720ef2b9dc848cc10da624530317eb
Serginho <yo@sergiocancelo.es>
2025-04-08 11:46:43 +0200

    feat: yarn command search scripts

--- ohmyzsh-custom/plugins/work/work.plugin.zsh
+++ ohmyzsh-custom/plugins/work/work.plugin.zsh
@@ -7,6 +7,32 @@ function __merge_staging() {
   git merge staging
 }
 
+function __package_script_select() {
+  local package_json="package.json"
+
+  if [[ ! -f "$package_json" ]]; then
+    echo "❌ No se encontró package.json en el directorio actual."
+    return 1
+  fi
+
+  local scripts=$(jq -r '.scripts | keys[]' "$package_json")
+  if [[ -z "$scripts" ]]; then
+    echo "⚠️ No se encontraron scripts en package.json."
+    return 1
+  fi
+
+  local selected=$(echo "$scripts" | fzf --prompt="Selecciona un script: ")
+
+  if [[ -z "$selected" ]]; then
+    echo "🚫 Cancelado."
+    return 0
+  fi
+
+  echo "🚀 Ejecutando: yarn $selected"
+  yarn "$selected"
+}
+
+
 alias reruntests="git commit --allow-empty --no-verify -m 'build: rerun tests' && git push"
 alias y='yarn'
 alias yct='yarn cli:test'
@@ -21,5 +47,6 @@ alias yibr='yarn ibazel run'
 alias yl='yarn lint'
 alias yi='yarn install'
 alias ys='yarn start'
+alias yscr=__package_script_select
 
 alias merge-staging=__merge_staging
